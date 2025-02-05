48e2fc6e41d80d27c95aae6eda2ed1d1f4def32a
Kexin <30476794+kk221@users.noreply.github.com>
2025-02-05 19:01:16 +0100

    Update Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="bg-purple-900 text-white mt-12 py-8">
-      <div className="container mx-auto px-4 text-center">
+    <footer>
+      <div>
         <p>© 2024 KarmaBeads. All rights reserved.</p>
       </div>
     </footer>
