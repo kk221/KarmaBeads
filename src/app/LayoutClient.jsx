f3c4e1b998c427c90fcf3366ae3da02b70f7d074
Kexin <30476794+kk221@users.noreply.github.com>
2025-02-09 13:53:37 +0100

    Update LayoutClient.jsx

--- src/app/LayoutClient.jsx
+++ src/app/LayoutClient.jsx
@@ -1,13 +1,15 @@
 'use client'
 
-import Header from './Header';
-import Footer from './Footer';
+import Header from './Header'
+import Footer from './Footer'
 
 export default function LayoutClient({ children }) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main className="container mx-auto px-4 pt-28 pb-16 md:pt-32">
+        {children}
+      </main>
       <Footer />
     </>
   );
