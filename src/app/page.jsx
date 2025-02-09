05dfa4eedc0050be72c7aa7af4ff450d7735601c
Kexin <30476794+kk221@users.noreply.github.com>
2025-02-09 14:15:14 +0100

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
-import DailyOracle from '@/components/shared/DailyOracle'
-import ServicesGrid from '@/components/shared/ServicesGrid'
+import DailyOracle from '../../components/shared/DailyOracle'
+import ServicesGrid from '../../components/shared/ServicesGrid'
 
 export default function Home() {
   return (
