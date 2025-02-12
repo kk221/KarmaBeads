8e9702a503c998a6546b96f76e3fd95794cfec22
Kexin <30476794+kk221@users.noreply.github.com>
2025-02-12 12:04:11 +0100

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,5 @@
-import DailyOracle from '/src/components/shared/DailyOracle'
+import DailyOracle from 'src/components/shared/DailyOracle'
 
 export default function Page() {
-  return (
-    <main className="min-h-screen bg-[#1d2a3a]">
-      <DailyOracle />
-    </main>
-  )
+  return <DailyOracle />
 }
